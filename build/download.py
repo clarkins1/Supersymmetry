c29c62d980ff53c3a9f036dba000d3c35b8b21a5
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2025-01-12 15:34:32 +0900

    why'd you have to exam and make things so completed

--- build/download.py
+++ build/download.py
@@ -6,7 +6,8 @@ import os
 
 basePath = os.path.normpath(os.path.realpath(__file__).replace("download.py", "") + "/server")
 
-with open(basePath + "/forge-installer.jar", "w+b") as jar:
+def download():
+    with open(basePath + "/forge-installer.jar", "w+b") as jar:
         forgeVer = "14.23.5.2860"
         mcVer = "1.12.2"
         url = (
@@ -22,25 +23,25 @@ with open(basePath + "/forge-installer.jar", "w+b") as jar:
         )
         r = requests.get(url)
         jar.write(r.content)
-print("Forge installer Downloaded")
+    print("Forge installer Downloaded")
 
-vanilla = basePath + "/minecraft_server.1.12.2.jar"
-if not os.path.isfile(vanilla):
-    with open(basePath + "/minecraft_server.1.12.2.jar", "w+b") as jar:
-        url = "https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar"
-        r = requests.get(url)
-        jar.write(r.content)
-print("Vanilla Downloaded")
+    vanilla = basePath + "/minecraft_server.1.12.2.jar"
+    if not os.path.isfile(vanilla):
+        with open(basePath + "/minecraft_server.1.12.2.jar", "w+b") as jar:
+            url = "https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar"
+            r = requests.get(url)
+            jar.write(r.content)
+    print("Vanilla Downloaded")
 
-subprocess.run(["java", "-jar", "forge-installer.jar",
+    subprocess.run(["java", "-jar", "forge-installer.jar",
                    "--installServer"], cwd=basePath)
-print("Forge Installed")
+    print("Forge Installed")
 
-try:
-    os.remove(basePath + "/forge-installer.jar")
-except Exception as e:
-    print("Couldn't delete forge-installer.jar: %s" % (e))
-try:
-    os.remove(basePath + "/forge-installer.jar.log")
-except Exception as e:
-    print("Couldn't delete forge-installer.jar.log: %s" % (e))
+    try:
+        os.remove(basePath + "/forge-installer.jar")
+    except Exception as e:
+        print("Couldn't delete forge-installer.jar: %s" % (e))
+    try:
+        os.remove(basePath + "/forge-installer.jar.log")
+    except Exception as e:
+        print("Couldn't delete forge-installer.jar.log: %s" % (e))
