01637f0405684a21ec978080b05a8d4f99d13a8d
MCTian_mi <3431493799@qq.com>
2025-01-17 10:11:48 +0800

    fix: this doesn't work™

--- groovy/postInit/mod/FluidloggedAPI.groovy
+++
@@ -1,9 +0,0 @@
-import git.jbredwards.fluidlogged_api.api.event.FluidloggableEvent
-import net.minecraftforge.fluids.FluidRegistry
-import net.minecraftforge.fml.common.eventhandler.Event
-
-event_manager.listen{FluidloggableEvent event ->
-    if (event.fluid == FluidRegistry.LAVA) {
-        event.setResult(Event.Result.DENY)
-    }
-}
